824f1454b43b207bdd653a57dc172a518219e2f5
satya-sairam <satyasairam144@gmail.com>
2021-10-29 19:23:44 +0530

    variables added and types

--- 02-varaibles/02-vars.tf
+++ 02-varaibles/02-vars.tf
@@ -35,3 +35,18 @@ variable "list" {
 output "list-output" {
   value = var.list
 }
+
+variable "list-map-variable" {
+  default = {
+    training = " devops with aws",
+    timing   = "7:30AM"
+  }
+}
+
+output "map-var-output" {
+  value = var.list-map-variable[training]
+}
+
+output "map-var-output2" {
+  value = var.list-map-variable
+}
