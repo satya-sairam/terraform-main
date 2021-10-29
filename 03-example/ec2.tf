7db9d50ad850a1a912977db35509917bc26957e5
satya-sairam <satyasairam144@gmail.com>
2021-10-29 21:00:27 +0530

    new instance using security group

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "sample" {
   ami                      = "ami-0e4e4b2f188e91845"
   instance_type            = "t2.micro"
+  vpc_security_group_ids   = [aws_security_group.SG.id]
   tags                     = {
     Name                   = "new"
   }
