8e403a6f8391db53c49e0c156c4dd8110b11b6a9
satya-sairam <satyasairam144@gmail.com>
2021-10-29 19:15:29 +0530

    variables added and types

--- 02-varaibles/02-vars.tf
+++ 02-varaibles/02-vars.tf
@@ -4,3 +4,26 @@ variable "sample" {
 output "sample_output" {
   value = var.sample
 }
+
+variable "string" {
+  default = "this is a string "
+}
+
+output "string-output" {
+  value = var.string
+}
+
+variable "number" {
+  default = 500
+}
+
+output "num-output" {
+  value = var.number
+}
+variable "boolean" {
+  default = "true"
+}
+
+output "boolean-output" {
+  value = var.boolean
+}
