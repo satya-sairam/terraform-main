48f2f61f531d2ecf46e3a6e05aac06b4c387fa41
satya-sairam <satyasairam144@gmail.com>
2021-10-30 17:11:39 +0530

    added modules

--- 05-modules/ec2/ec2instance.tf
+++ 05-modules/ec2/ec2instance.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "demo" {
+  count                    = 2
   ami                      = "ami-0e4e4b2f188e91845"
   instance_type            = "t2.micro"
   vpc_security_group_ids   = [var.Sec_grp]
